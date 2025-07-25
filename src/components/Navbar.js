398cf36bc9cfe9fbd389b7e0750a0f9e3e1c33a5
skmojahidulislam <skmojahidulislam298@gmail.com>
2025-07-25 15:13:27 +0530

    changes in navbar 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => (
     <div className="logo">👟 FASHION SSR</div>
     <nav>
       <ul>
-        <li><a href="about">About</a></li>
+        <li><a href="home">Home</a></li>
         <li><a href="women">Women</a></li>
         <li><a href="men">Men</a></li>
         <li><a href="shoes">Shoes</a></li>
